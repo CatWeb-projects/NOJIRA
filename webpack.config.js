ff7aa5859d5bec9fd5028bbc5308371fab60bf16
Tudor Uzun <tudor@Tudors-MacBook-Pro.local>
2021-03-03 17:38:26 +0200

    NOJIRA-1: webpack config not completed

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,51 @@
-const webpack = require("webpack");
-const browserSync = require("browser-sync");
-const server = browserSync.create();
+const path = require("path");
+const fs = require("fs");
 
-webpack.task("reload", (done) => {
-  server.reload();
-  done();
-});
+// const HWPConfig = [];
+// fs.readdirSync(path.resolve(__dirname, "../src/pages")).forEach((file) => {
+//   page = new HtmlWebpackPlugin({
+//     filename: file.replace(".twig", ".html"),
+//     template: path.resolve(__dirname, `../src/pages/${file}`),
+//     hash: true,
+//   });
+//   HWPConfig.push(page);
+// });
+
+module.exports = {
+  mode: "development",
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "index.bundle.js",
+  },
+  devServer: {
+    port: 3000,
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          "autoprefixer",
+          "css-loader",
+          "node-sass",
+          { loader: "sass-loader", options: { sourceMap: true } },
+        ],
+      },
+      {
+        test: /\.twig$/,
+        use: {
+          loader: "twig-html-loader",
+        },
+      },
+    ],
+  },
+  // plugins: [...HWPConfig],
+};

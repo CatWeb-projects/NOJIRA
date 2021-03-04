2f1ca19a09a9716c7a7c748206ff0bc09328f748
Tudor Uzun <tudor@Tudors-MacBook-Pro.local>
2021-03-04 12:43:58 +0200

    NOJIRA-1: base64 encoding

--- webpack.config.js
+++ webpack.config.js
@@ -48,8 +48,11 @@ module.exports = {
           loader: "twig-html-loader",
         },
       },
+      {
+        test: /\.(jpe?g|png|ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+        use: "base64-inline-loader?limit=1000&name=[name].[ext]",
+      },
     ],
   },
-  watch: true,
   plugins: [new HtmlWebpackPlugin()],
 };
